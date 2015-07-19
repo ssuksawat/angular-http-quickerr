b75906d9339493eec00fe19f85d5b7afebfaf5c7
ssuksawat@worcester.edu <ssuksawat21@gmail.com>
2015-07-19 19:56:26 -0400

    [sompop] working quickErrProvider & quickErr factory

--- src/angular-http-quickerr.js
+++ src/angular-http-quickerr.js
@@ -1,21 +1,38 @@
+/**
+ * Author: Sompop Suksawat (ssuksawat21@gmail.com)
+ */
+
 (function(){
 	
 	'use strict';
 	
 	angular.module('ngHttpQuickErr', [])
 		.provider('quickErr', function($provide) {
+			var config = {
+				key: 'status'
+			};
 			var handlers = {
-				GLOBAL: {},
+				GLOBAL: {
+					DEFAULT: {
+						logger: console.error,
+						template: '{{status}}: {{description}}!'
+					}
+				},
 				CUSTOM: {}
 			};
 			
 			return {
+				setResponseFormat: setResponseConfig,
 				setGlobalHandlers: setGlobalHandlers,
 				setCustomHandlers: setCustomHandlers,
 				$get: instantiateFactory
 			};
 			
 			/* ----- PUBLIC ----- */
+			
+			function setResponseConfig(newConfig) {
+				angular.extend(config, newConfig);
+			}
 			
 			function setGlobalHandlers(newHandlers) {
 				angular.extend(handlers.GLOBAL, newHandlers);
@@ -27,18 +44,88 @@
 			
 			/* ----- FACTORY API ----- */
 			
-			function instantiateFactory() {
+			function instantiateFactory($q, $location) {
 				var service = {
 					handle: handle
 				};
 				
 				return service;
 				
-				/* ----- PUBLIC ----- */
+				/* ----- FACTORY PUBLIC ----- */
 				
-				function handle() {
-					console.log('print handlers: ', handlers);
+				function handle(res, customType, callback) {
+					var message, logger, postLog;
+					
+					if (res) {
+						// Retrieve all parameters
+						message = getTemplate(res, customType);
+						message = interpolateMessage(message, res);
+						logger = getLogger(res, customType);
+						postLog = getPostLogAction(res, customType);
+						
+						// Log error message
+						logger(message, res);
+						
+						// Either callback or 'post-log' action
+						if (callback && typeof callback === 'function') {
+							callback(res);
+						} else if (postLog) {	//Perform predefined post-log action
+							// rethrow error promise
+							if (typeof postLog === 'boolean') {		
+								return $q.reject(res);
+							}
+							// redirect to specified path
+							if (typeof postLog === 'string' && postLog.length > 0) {
+								$location.path(postLog);
+							}
+							// call predefined postlog function
+							if (typeof postLog === 'function') {
+								postLog(res);
+							}
+						}					
+					} else {
+						return $q.reject();		//rethrow the rejected promise if response is undefined
+					}
 				}
+				
+				/* ----- FACTORY PRIVATE ----- */
+				
+				function interpolateMessage(str, res) {
+					return str.replace(/{{(.*?)}}/g, function (_, prop) {
+						return res[prop] || '';
+					});
+				}
+				
+				function getTemplate(res, customType) {
+					if (customType && handlers.CUSTOM[customType][res[config.key]].template) {
+						return handlers.CUSTOM[customType][res[config.key]].template;
+					} else if (handlers.GLOBAL[res[config.key]].template) {
+						return handlers.GLOBAL[res[config.key]].template;
+					} else {
+						return handlers.GLOBAL.DEFAULT.template;
+					}
+				}
+				
+				function getLogger(res, customType) {
+					if (customType && handlers.CUSTOM[customType][res[config.key]].logger) {
+						return handlers.CUSTOM[customType][res[config.key]].logger;
+					} else if (handlers.GLOBAL[res[config.key]].logger) {
+						return handlers.GLOBAL[res[config.key]].logger;
+					} else {
+						return handlers.GLOBAL.DEFAULT.logger;
+					}
+				}
+				
+				function getPostLogAction(res, customType) {
+					if (customType && handlers.CUSTOM[customType][res[config.key]].postLog) {
+						return handlers.CUSTOM[customType][res[config.key]].postLog;
+					} else if (handlers.GLOBAL[res[config.key]].postLog) {
+						return handlers.GLOBAL[res[config.key]].postLog;
+					} else {
+						return ;
+					}
+				}
+				
 			}
 			
 		});
--- test/angular-http-quickerr.spec.js
+++ test/angular-http-quickerr.spec.js
@@ -2,37 +2,119 @@
 
 describe('Angular Http QuickErr', function() {
 	
-	describe('Using GLOBAL handlers', function() {
+	describe('Using quickErr handlers', function() {
 		
-		beforeEach(module('ngHttpQuickErr', function(quickErrProvider) {
-			quickErrProvider.setGlobalHandlers({
-					logger: 'after',
-					400: {
-						message: 'This is a 400 error',
-						rethrow: true
-					},
-					401: {
-						message: 'This is a 401 error',
-						redirect: '/login'
-					},
-					500: {
-						message: 'This is a 500 error',
-						callback: function() { console.log('callback 500 called'); }
-					}
-				});
+		var quickErr, 
+				mockCustomLogger, globalSetup, customSetup,
+				mockQ, mockLocation;		//Angular dependencies
+		
+		// Set up spies
+		beforeEach(function () {
+			mockCustomLogger = {error: sinon.spy()};
+			mockQ = {reject: sinon.spy()};
+			mockLocation = {path: sinon.spy()};
+			
+			sinon.spy(console, 'error');
+		})
+		
+		// Set up module & sample templates
+		beforeEach(module('ngHttpQuickErr', function ($provide, quickErrProvider) {
+			globalSetup = {
+				400: {
+					template: 'This is default message without interpolation.'
+				},
+				401: {
+					template: 'Hello, User! >>> {{description}}',
+					postLog: '/login'
+				},
+				404: {
+					template: 'Hello, User! This is a {{status}} error.',
+					postLog: true
+				},
+				500: {
+					template: 'Using custom logger for this error.',
+					postLog: function () { console.log('I am a post-log function!'); },
+					logger: mockCustomLogger.error
+				},
+				501: {
+					template: 'This is message should NOT be displayed -> override by CUSTOM.'
+				}
+			};
+			
+			customSetup = {
+				'Namespace': {
+					501: { template: 'This is {{status}} in CUSTOM.' }
+				}
+			}
+			
+			quickErrProvider.setGlobalHandlers(globalSetup);	
+			quickErrProvider.setCustomHandlers(customSetup);
+			
+			$provide.value('$q', mockQ);
+			$provide.value('$location', mockLocation);
 		}));
 		
-		var quickErr;
-		beforeEach(inject(function(_quickErr_) {
+		// Inject service
+		beforeEach(inject(function (_quickErr_, _$q_, _$location_) {
 			quickErr = _quickErr_;
 		}));
 		
-		it('should say "hi"', function() {
-			console.log('hi');
-			quickErr.handle();
+		afterEach(function () {
+			console.error.restore();
+		})
+		
+		it('should log normal message without extra options', function () {
+			var testRes = {status: 400, description: 'Should NOT log this' };
 			
-			expect(true).toBeTruthy();
+			quickErr.handle(testRes);
+			
+			expect(console.error.calledWith(globalSetup['400'].template, sinon.match.object)).toBeTruthy();
 		});
+		
+		it('should re-route to "/login" after logging', function () {
+			var testRes = {status: 401, description: 'Hello 401'};
+			
+			quickErr.handle(testRes);
+			
+			expect(console.error.calledWith('Hello, User! >>> Hello 401', sinon.match.object)).toBeTruthy();
+			expect(mockLocation.path.calledWith('/login')).toBeTruthy();
+			expect(mockQ.reject.called).not.toBeTruthy();
+		});
+		
+		it('should rethrow rejected promise after logging', function () {
+			var testRes = {status: 404};
+			
+			quickErr.handle(testRes);
+			
+			expect(console.error.calledWith('Hello, User! This is a 404 error.', sinon.match.object)).toBeTruthy();
+			expect(mockLocation.path.called).not.toBeTruthy();
+			expect(mockQ.reject.called).toBeTruthy();
+		});
+		
+		it('should use custom logger & call predefined "post-logging" callback', function () {
+			var testRes = {status: 500};
+			sinon.spy(console, 'log');
+
+			quickErr.handle(testRes);
+			
+			expect(mockCustomLogger.error.calledWith('Using custom logger for this error.', sinon.match.object)).toBeTruthy();
+			expect(mockLocation.path.called).not.toBeTruthy();
+			expect(mockQ.reject.called).not.toBeTruthy();
+			expect(console.log.calledWith('I am a post-log function!')).toBeTruthy();
+			
+			console.log.restore();
+		});
+		
+		it('should use CUSTOM w/ Namespace handler', function () {
+			var testRes = {status: 501};
+			
+			quickErr.handle(testRes, 'Namespace');
+			
+			expect(console.error.calledWith('This is 501 in CUSTOM.', sinon.match.object)).toBeTruthy();
+			expect(mockLocation.path.called).not.toBeTruthy();
+			expect(mockQ.reject.called).not.toBeTruthy();
+		});
+		
 	});
 	
 });
